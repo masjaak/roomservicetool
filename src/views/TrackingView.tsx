2686fecac212f490353081ad70e2b72b70843ee0
DaddyMissWar <masjaak@gmail.com>
2026-04-13 03:34:53 +0700

    fix the deploy eror

--- src/views/TrackingView.tsx
+++ src/views/TrackingView.tsx
@@ -136,6 +136,7 @@ export const TrackingView: React.FC<TrackingViewProps> = ({ roomNumber, onFinish
                       </p>
                     </div>
                   </div>
+                </div>
               );
             })}
           </div>
